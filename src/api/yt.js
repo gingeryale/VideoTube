55ca951734944bfc89ae42a9e5a44c688fa07781
ao <aolin@ymail.com>
2020-10-29 17:32:09 +0200

    mod key

--- src/api/yt.js
+++ src/api/yt.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyATeUGRc4QzYNvc4vavK7fYcDxe-H27CzI';
+const KEY = 'process.env.KEY';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/',
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,7 +3,7 @@ import './VideoItem.css';
 
 const VideoItem = ({ videoItem }) => {
     return (
-        <div className="item">
+        <div className="item video-item">
             <img className="ui image" src={videoItem.snippet.thumbnails.medium.url} alt="videoItem.snippet.description" />
             <div className="content">
                 <h3 className="header">{videoItem.snippet.title}</h3>
