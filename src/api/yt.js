9e9c544f8ba76abd59596d02f104796872306644
ao <aolin@ymail.com>
2020-10-30 10:34:34 +0200

    mask KEY

--- src/api/yt.js
+++ src/api/yt.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-
-const KEY = 'AIzaSyATeUGRc4QzYNvc4vavK7fYcDxe-H27CzI';
+// restricted Google API key
+import KEY from './env';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/',
@@ -8,6 +8,6 @@ export default axios.create({
         part: 'snippet',
         type: 'video',
         maxResults: 5,
-        key: KEY
+        key: `${KEY}`
     }
 });
